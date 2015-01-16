29724d2f257081885537e6346a37146a80010b28
Vishu Mahabir <vishu.mahabir@gmail.com>
2015-01-16 11:18:37 +0100

    added yeoman angular gen

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name photoUploaderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the photoUploaderApp
+ */
+angular.module('photoUploaderApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name photoUploaderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the photoUploaderApp
+ */
+angular.module('photoUploaderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
